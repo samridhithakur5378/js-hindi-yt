8f2c24727d96d63227bd65e9e5e7b692b3a59251
samridhi <samridhithakur5378@gmail.com>
2026-06-28 11:47:42 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+//primitive
+
+//7 types:string,number,boolean,null,undefined,Symbol,Bigint
+
+const score=100
+//javascript is dynamically typed taht is datatypes are not explicitly defined beforehand
+
+
+const scoreValue=100.3
+
+const isLoggedIn=false
+
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+const bigNumber=35445368979798786675634212123n;
+
+//reference/non-primitive
+
+
+
+//arrays,objects,functions
+
+const heroes=['shaktiman','doga','naagraj']
+let myObj={
+   name:"hitesh"
+
+
+}
+
+const myFunction =function(){}
+console.log("hello world");
+
+
+console.log(typeof myFunction)

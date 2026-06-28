158a552e4d38344bce9742327e29ae9d9d5ccb90
samridhi <samridhithakur5378@gmail.com>
2026-06-28 00:15:12 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,30 @@
+let score=true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+
+
+let isLoggedIn='hitesh'
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//0=>false
+//1=>true
+
+//""=>false
+//"samridhi"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

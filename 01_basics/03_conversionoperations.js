0702b90eece5934f94bfd2c6cb8f205aa6ddb138
samridhi <samridhithakur5378@gmail.com>
2026-06-28 01:29:45 +0000

    comparison operators

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -28,3 +28,47 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+
+///******************conversion*******************************
+let value=3
+let negValue=-value
+console.log(negValue)
+
+
+
+console.log(3+3)
+console.log(3-3)
+console.log(3*3)
+console.log(3**3)
+console.log(9/3)
+console.log(9%3)
+
+let str1="hello"
+let str2="hitesh"
+let str3= str1+str2;
+console.log(str3)
+
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+//reason for different output is that if string is first then  treated like a string
+console.log(1+2+"2");
+//here integer is first therefore firtly addition takes place
+
+console.log(+true)
+console.log(+"")
+
+//dont write code like below
+
+
+
+
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)

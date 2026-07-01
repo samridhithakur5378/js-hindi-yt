84eb46fc73ed7a5e21b71fe24da967a8c6989be2
samridhi <samridhithakur5378@gmail.com>
2026-07-01 02:04:27 +0000

    object 1 completed

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,6 +2,10 @@
 //when we declate as literal then not singleton
 //if declared by contructor then singleton
 
+//how to declare symbol
+//iportant interview question
+const mySym = Symbol("key1")
+
 
 //object literals
 //Object.create
@@ -9,7 +13,7 @@ const JsUser ={
 name:"Hitesh",
 "fullname":"hitesh choudhary",
 age:18,
-[mySym]:"mykey1",//You need to write it in single block bracket to use as symbol or else it willbe a string
+[mySym]:"mykey1",//(You need to write it in single block bracket to use as symbol or else it willbe a string)
 location:"jaipur",
 email:"sam@gmail.com",
 isloggedin:false,
@@ -23,8 +27,25 @@ lastLogindays:["monday","saturday"]
  console.log(JsUser.email)
 console.log(JsUser["fullname"])
 
-//how to declare symbol
-//iportant interview question
-const mySym = Symbol("key1") 
 
-console.log( typeof JsUser.mySym)
+
+console.log( JsUser[mySym])
+
+
+
+JsUser.email = "hitesh@chatgpt.com"
+//Object.freeze(JsUser) // if you freeze something then its value cannot be changed
+
+
+
+JsUser.email = "hi@gamail.com"
+//console.log(JsUser)
+
+
+JsUser.greeting = function(){console.log("hello JS user")}
+console.log(JsUser.greeting())
+
+JsUser.greetings2= function(){
+console.log("hello JS user, ${this.name}")}//string interpolation is used here
+console.log(JsUser.greetings2())
+

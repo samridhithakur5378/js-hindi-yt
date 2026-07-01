04f2e48e70f11ea0399be932686a013311869cbd
samridhi <samridhithakur5378@gmail.com>
2026-07-01 05:56:42 +0000

    objects part 2 done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,30 @@
+//write 
+
+
+//destructuring
+const course = {
+coursename :"js in hindi",
+price:"999",
+courseInstructor:"hitesh"
+}
+
+
+//course.courseInstructor
+//below shows how to destructure:
+const {courseInstructor:instructor} = course
+console.log(instructor)
+
+
+const navbar  = ({company}) ={}
+
+navbar(company = "hitesh")
+
+//{
+ //"name":"hitesh",
+ //"coursename":"js in hindi",
+ //"price":"free"
+//}
+
+[{
+
+}]

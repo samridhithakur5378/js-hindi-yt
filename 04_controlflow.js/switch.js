2aa3b051d9db0e2d9d1318f42f6b7207c022eee2
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-07-03 18:28:45 +0530

    switch done

---
+++ 04_controlflow.js/switch.js
@@ -0,0 +1,32 @@
+//syntax for switch
+//switch(key){
+ // case value:
+
+ // break;
+
+  //default:
+   // break;
+
+//}
+
+const month = "april"
+switch(month){
+ case "january":
+console.log("january")
+  break;
+case "feb":
+console.log("february")
+  break;
+  case "m":
+console.log("march")
+ break;
+ case "april":
+console.log("april")
+  break; 
+
+
+  default:
+    console.log("default case match")
+    break;
+
+}

82cfab87c98918bb6434cfb8055401443c8560a4
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-07-03 20:35:50 +0530

    completed for continue and break

---
+++ 05_iterations/one.js
@@ -0,0 +1,54 @@
+//for
+
+for (let index = 0; index <=10; index++) {
+  const element = index;
+  if (element ==5){
+    console.log("5 is the best number");
+  }
+  //console.log(element);
+  
+}
+
+for (let i= 0; i<= 10; i++) {
+  console.log(`outer loopvalue:${i}`);
+
+  for(let j=0;  j<= 10;j++)
+   // console.log(`inner loop value ${j} and outer loop ${i}`)
+  console.log(i + '*' + j +'='+ i*j);
+ 
+  
+}
+
+let myArray = ["flash","batman","superman"]
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+  
+}
+
+
+//break and continue
+
+
+for (let index = 1; index <= 20; index++) {
+  if(index==5){
+    console.log(`Detected 5`);
+    break//used to break control  flow or loop
+  }
+  console.log(`Value of i is ${index}`)
+  
+}
+
+
+
+for (let index = 1; index <= 20; index++) {
+  if(index==5){
+    console.log(`Detected 5`);
+   continue//skips one condition for 1 time
+  }
+  console.log(`Value of i is ${index}`)
+  
+}
+
+
+

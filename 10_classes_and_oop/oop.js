2f63960ce13fd96dc529bb51dfe1d83832df7f8c
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-07-12 18:04:15 +0530

    oops done

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -17,15 +17,25 @@ console.log(user.username)
 //console.log(this)
 
 
-//constructor function gives you a new copy originals do not get affected by it
-
+//constructor function gives you a new copy/instance originals do not get affected by it
+//new keyword used to get new instance
 function User(username,loginCount,isLoggedIn){
   this.username =  username,
   this.loginCount=loginCount,
   this.isLoggedIn = isLoggedIn
 
+  this.greeting = function(){
+    console.log(`Welcome ${this.username}`)
+  }
   return this
 }
 const userOne =  new User("hitesh",12,true)
 const userTwo =  new User("Chai aur code",11,false)
-console.log(userOne)
+console.log(userOne.constructor)
+//console.log(userTwo)
+
+//whenever new keyword  is used new object is created
+//constructor function is called due to new keyword
+//this keyword will get injected in these objects 
+//then you get these in function
+//constructor proprty is just a refernce to the function taht created the instance

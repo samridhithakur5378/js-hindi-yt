f2cd4e3ddeb880cd7b60a0f2940040f881cae15c
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-07-13 23:33:48 +0530

    prototype

--- 10_classes_and_oop/Object.js
+++ 10_classes_and_oop/Object.js
@@ -11,4 +11,16 @@ console.log(multipleBy5.prototype);
 
 function createUser(username,score){
  this.username = username
+ this.score =score
 }
+ createUser.prototype.increment = function(){
+  this.score++//this matlab jis
+ }
+  createUser.prototype.printMe = function(){
+    console.log(`prize is ${this.score}`);
+  }
+ 
+ const chai = new createUser("chai", 25)//new keyword importance interview question
+ const tea = new createUser('tea',30)
+
+chai.printMe()
---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,14 @@
+let myName = "hitesh     "
+let myChannel = "chai   "
+//console.log(myName.trueLength)
+
+
+//console.log(myName.trim().length)
+
+let myHeroes = ['ironman','thor']
+let heroPower = {
+ ironman:'suit',
+  thor:'hammer',
+  
+getSuitPower:
+}

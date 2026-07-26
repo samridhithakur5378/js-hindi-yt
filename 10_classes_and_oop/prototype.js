84e79977b28a0e88a5e4d2423df215b4ab3988e8
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-07-26 10:14:28 +0530

    protostype done but still soem error

--- 10_classes_and_oop/prototype.js
+++ 10_classes_and_oop/prototype.js
@@ -5,10 +5,59 @@ let myChannel = "chai   "
 
 //console.log(myName.trim().length)
 
-let myHeroes = ['ironman','thor']
+let myHeroes = ['thor','spiderman']
 let heroPower = {
- ironman:'suit',
+
   thor:'hammer',
+  spiderman:'sling',
   
-getSuitPower:
+getSuitPower:function(){
+  console.log('spidy poer is ${this.spiderman}');
 }
+}
+Object.prototype.hitesh = function(){
+  console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh=function(){
+  console.log(`hitesh says hello`)
+}
+//not getting printed why??
+//heroPower.hitesh()
+//myHeroes.hitesh()
+//myHeroes.heyHitesh()
+//heroPower.heyHitesh()
+
+
+//inheritance
+const User = {
+  name:'chai',
+  email:"chai@gamil.com"
+
+}
+const Teacher = {
+  makeVideo :true
+}
+const TeachingSupport={
+  isAvailable:false
+}
+const TASupport = {
+  makeAssignments:'JS asignment',
+  fullTime:true ,
+  __proto__:TeachingSupport
+}
+
+Teacher.__proto__ =User
+//modern syntax
+Object.setPrototypeOf(TeachingSupport,Teacher)
+
+let anotherUsername ="chaiaurcode"
+String.prototype.trueLength = function(){
+  console.log(`${this}`)
+   console.log(`${this.name}`)
+   console.log(`True length is ${this.trim().length}`)
+}
+anotherUsername.trueLength()
+//ok so we are not getting output again why check revise this lec
+"hitesh".trueLength()
+"icetea".trueLength()
